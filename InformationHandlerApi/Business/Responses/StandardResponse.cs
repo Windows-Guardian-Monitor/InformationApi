dc46da455f80dc9974a2c5856147b79df985a87e
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2024-07-20 17:51:01 -0300

    Criar resposta padrão.

---
+++ InformationHandlerApi/Business/Responses/StandardResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace InformationHandlerApi.Business.Responses
+{
+    public class StandardResponse
+    {
+        public HttpStatusCode Code { get; set; }
+        public string Message { get; set; }
+    }
+}
